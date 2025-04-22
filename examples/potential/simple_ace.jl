86ec4c4c4a6071ec7ead5b38885fb4d1b03c0163
Christoph Ortner <christophortner0@gmail.com>
2025-04-22 10:44:42 -0700

    progress on ace example

--- examples/potential/simple_ace.jl
+++ examples/potential/simple_ace.jl
@@ -1,6 +1,11 @@
 # This example is a brief demonstration how to build an ACE-like 
 # O(3)-invariant model "by hand" (as opposed to via an ML framework). 
 
+import Polynomials4ML as P4ML 
+import EquivariantTensors as ET
+using StaticArrays
+
+##
 module ACE0 
 
 import Polynomials4ML as P4ML 
@@ -60,19 +65,60 @@ end
 end
 
 
-# to construct the ACE model we 
+## 
+# CONSTRUCTION OF THE ACE MODEL 
 
-Rn = legendre_basis(totdeg)
-Ylm = RYlmBasis(maxL)
-ν = 2
+# Some model parameters that we will use: 
+Dtot = 8   # total degree; specifies the trunction of embeddings and correlations
+maxL = 5    # maximum degree of spherical harmonics 
+ORD = 3     # correlation-order (body-order = ORD + 1)
 
-# Pooling and SparseProduct + n-corr 
-spec1p = [(i, y) for i = 1:totdeg for y = 1:maxL]
-bA = P4ML.PooledSparseProduct(spec1p)
+# [1] first specify the radial and angular embeddings 
+rbasis = P4ML.legendre_basis(Dtot+1)
+ybasis = P4ML.real_sphericalharmonics(maxL)
 
-# define n-corr spec
-tup2b = vv -> [ spec1p[v] for v in vv[vv .> 0]  ]
-admissible = bb -> ((length(bb) == 0) || (sum(b[1] - 1 for b in bb ) < totdeg)) # cannot use <= since we cannot approxiate poly basis corresponding to (2, 15) with (15)
-filter = bb -> (length(bb) == 0 || sum(idx2lm(b[2])[1] for b in bb) <= maxL)
-specAA = gensparse(; NU = ν, tup2b = tup2b, admissible = admissible, filter = filter, minvv = fill(0, ν), maxvv = fill(length(spec1p), ν), ordered = true)
-spec = [ vv[vv .> 0] for vv in specAA if !(isempty(vv[vv .> 0]))]
+# [2] Pooling and SparseProduct
+# this layer takes the embeddings of the individual particles and pools them 
+# to embed the entire set of particles. (point cloud) Note this is a sparse 
+# operation; only the basis functions Aₙₗₘ are computed for which n + l ≤ Dtot.
+#
+Aspec = [ (n+1, P4ML.lm2idx(l, m)) 
+           for n = 0:Dtot for l = 0:maxL for m = -l:l if (n + l <= Dtot) ]
+abasis = ET.PooledSparseProduct(Aspec)
+@assert abasis.spec == Aspec
+
+# [3] n-correlations 
+# generating sparse n-correlations is a little more involved, and here is it 
+# better to just automate this. But for a very small model we can still do it 
+# by hand. 
+# first get all possible combinations of A basis functions, then we will filter 
+comb1 = with_replacement_combinations(0:length(Aspec), ORD)
+ii2bb = ii -> begin 
+      bb = [ Aspec[i] for i in ii[ii .> 0]  ];
+      nn = [b[1]-1 for b in bb]; 
+      ll = [P4ML.idx2lm(b[2])[1] for b in bb];
+      mm = [P4ML.idx2lm(b[2])[2] for b in bb];
+      return nn, ll, mm 
+   end
+myfilter = ii -> begin 
+      nn, ll, mm = ii2bb(ii);
+      return ( (sum(nn + ll; init=0) <= Dtot) &&  # total degree trunction
+               iseven(sum(ll; init=0)) &&         # reflection-invariance
+               sum(mm; init=0) == 0 );            # rotation-invariance
+   end 
+
+@show length(comb1)
+comb2 = Base.filter(myfilter, collect(comb1)) 
+@show length(comb2) 
+
+# notice the incredible reduction in the number of features due to imposing 
+# the filters given by the O(3) invariance constraints and the sparsification
+# (the latter can be thought of as a smoothness prior)
+
+# to finish the 𝔸spec we need to convert to 0-corr, 1-corr, 2-corr and 3-corr
+# by dropping the zeros from the combinations 
+𝔸spec = [ filter(!iszero, ii) for ii in comb2 ]
+# and now we can finally generate the n-correlations layer 
+aabasis = ET.SparseSymmProd(𝔸spec)
+
+# [4] symmetrization
