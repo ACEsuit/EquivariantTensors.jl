a3b0dc9719c948d7f61753e7fd0a49faef1ee334
Christoph Ortner <christophortner0@gmail.com>
2025-04-21 21:00:31 -0700

    start building an example model

---
+++ examples/potential/simple_mlip.jl
@@ -0,0 +1,12 @@
+# This example is a brief demonstration how to build an ACE-like 
+# O(3)-invariant model. 
+#
+
+module MiniACE0 
+
+using Polynomials4ML, EquivariantTensors, StaticArrays 
+
+
+
+end
+
