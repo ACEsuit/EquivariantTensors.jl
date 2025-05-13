eb8017ee2b6532f3213926c9f55ad3cf33054577
Liwei Zhang <68692847+zhanglw0521@users.noreply.github.com>
2025-05-13 11:50:17 +0200

    Add rAA2cAA test

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,6 +21,7 @@ end
     @testset "SYYVector" begin include("test_yyvector.jl"); end
     @testset "Clebsch Gordan Coeffs" begin include("test_clebschgordans.jl"); end
     @testset "Representation" begin include("test_representation.jl"); end
+    @testset "Real AA to Complex AA" begin include("test_rAA2cAA.jl"); end
     @testset "Coupling Coeffs" begin include("test_coupling.jl"); end
 end 
 
