987bea9da76ecf0572bbaaaa396906a9a2ec544d
Liwei Zhang <68692847+zhanglw0521@users.noreply.github.com>
2025-05-14 10:30:15 +0200

    Modify doc

--- examples/ace/simple_ace_equivariant.jl
+++ examples/ace/simple_ace_equivariant.jl
@@ -98,8 +98,7 @@ Q𝐫 = Ref(Q) .* 𝐫[perm]
 @show DQ * φ ≈ φQ
 
 const A = [0 0 1; 1 0 0; 0 1 0] 
-# A is the transformation from SpheriCart rSH to CondonShortley rSH
-# while CondonShortley rSH is actually a Cartesian basis
+# A is the transformation from SpheriCart rSH to CondonShortley rSH + a fixed transformation
 _evaluate(m::SimpleACE3, 𝐫::AbstractVector{<: SVector{3}}) = A * evaluate(m, 𝐫)
 
 φ  = _evaluate(model, 𝐫)
