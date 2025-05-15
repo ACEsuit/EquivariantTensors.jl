5f8c8b7a00758c25129217659f00d1b7c1c56d3d
Liwei Zhang <68692847+zhanglw0521@users.noreply.github.com>
2025-05-15 17:21:48 +0200

    doc update

--- src/O3/O3.jl
+++ src/O3/O3.jl
@@ -11,6 +11,21 @@ include("O3_utils.jl")
 include("yyvector.jl")
 include("O3_transformations.jl")
 
+
+"""
+    O3.coupling_coeffs(L, ll, nn; PI, basis)
+    O3.coupling_coeffs(L, ll; PI, basis)
+
+Compute coupling coefficients for the spherical harmonics basis, where 
+- `L` must be an `Integer`;
+- `ll, nn` must be vectors or tuples of `Integer` of the same length.
+- `PI`: whether or not the coupled basis is permutation-invariant (or the 
+corresponding tensor symmetric); default is `true` when `nn` is provided 
+and `false` when `nn` is not provided.
+- `basis`: which basis is being coupled, default is `complex`, alternative
+choice is `real`, which is compatible with the `SpheriCart.jl` convention.  
+"""
+
 coupling_coeffs(L::Integer, ll, nn = nothing; 
                          PI = !(isnothing(nn)), 
                          basis = complex) = 
