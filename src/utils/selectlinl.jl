3a166faff727607d7294ef00f11f937e3e5e98ca
Christoph Ortner <christophortner0@gmail.com>
2025-11-21 02:10:19 -0800

    bugfix selectlinl

--- src/utils/selectlinl.jl
+++ src/utils/selectlinl.jl
@@ -24,9 +24,21 @@ function LuxCore.initialparameters(rng::AbstractRNG, l::SelectLinL)
    return (W = W,)
 end
 
-function (l::SelectLinL)( P_X, ps, st)
-   P, X = P_X
+(l::SelectLinL)( P_X, ps, st) = 
+      _apply_selectlinl(l, P_X[1], P_X[2], ps, st)
+
+# TODO: this case distinction is a hack; it would be better to 
+#       use types one cannot iterate over. Something to look into. 
+#       and an argument to move to DecoratedParticles.jl
+
+function _apply_selectlinl(l, P, X::AbstractArray, ps, st)
    B = reduce(vcat, transpose(ps.W[:, :, l.selector(x)] * P[i, :])
                     for (i, x) in enumerate(X) )
+   return B, st 
+end
+
+function _apply_selectlinl(l, P, x::Union{NamedTuple, Number, StaticArray},
+                           ps, st)
+   B = ps.W[:, :, l.selector(x)] * P
    return B, st 
 end
